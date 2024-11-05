2ff67cdb81140be80ecf71207125a89b6d02f965
thislife_won <sw8744@naver.com>
2024-11-05 20:59:43 +0900

    Add flowRate

--- frontend/src/pages/flowRate/flowRate.js
+++ frontend/src/pages/flowRate/flowRate.js
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './flowRate.css';
 
 const { kakao } = window;
 
 function Flowrate() {
+    const [value, setValue] = useState(5);
     const navigate = useNavigate();
     const goMain = () => {
         navigate('/');
@@ -15,19 +16,19 @@ function Flowrate() {
 
     useEffect(() => {
         const overlayCoords = [
-            [new kakao.maps.LatLng(36.35093930118267, 127.30023814907075), 0],
-            [new kakao.maps.LatLng(36.35093930118267, 127.30013814907075), 0],
-            [new kakao.maps.LatLng(36.35093930118267, 127.30003814907075), 0],
-            [new kakao.maps.LatLng(36.35093930118267, 127.29993814907075), 0],
-            [new kakao.maps.LatLng(36.35093930118267, 127.29983814907075), 0],
-            [new kakao.maps.LatLng(36.35093930118267, 127.29973814907075), 0],
-            [new kakao.maps.LatLng(36.35093930118267, 127.29963814907075), 0],
-            [new kakao.maps.LatLng(36.35093930118267, 127.29953814907075), 0],
-            [new kakao.maps.LatLng(36.35093930118267, 127.29943814907075), 0],
-            [new kakao.maps.LatLng(36.35093930118267, 127.29933814907075), 0],
-            [new kakao.maps.LatLng(36.35093930118267, 127.29923814907075), 0],
-            [new kakao.maps.LatLng(36.35093930118267, 127.2990794907075), 1],
-            [new kakao.maps.LatLng(36.35111930118267, 127.2990794907075), 1],
+            [new kakao.maps.LatLng(36.35093930118267, 127.30003814907075)],
+            [new kakao.maps.LatLng(36.35093930118267, 127.29983814907075)],
+            [new kakao.maps.LatLng(36.35093930118267, 127.29963814907075)],
+            [new kakao.maps.LatLng(36.35093930118267, 127.29943814907075)],
+            [new kakao.maps.LatLng(36.35093930118267, 127.29923814907075)],
+            [new kakao.maps.LatLng(36.35093930118267, 127.2990794907075)],
+            [new kakao.maps.LatLng(36.35111930118267, 127.2990794907075)],
+            [new kakao.maps.LatLng(36.35131930118267, 127.2990794907075)],
+            [new kakao.maps.LatLng(36.35151930118267, 127.2990794907075)],
+            [new kakao.maps.LatLng(36.35171930118267, 127.2991494907075)],
+            [new kakao.maps.LatLng(36.35193930118267, 127.2992794907075)],
+            [new kakao.maps.LatLng(36.35216030118267, 127.2994294907075)],
+            [new kakao.maps.LatLng(36.35225530118267, 127.2996194907075)],
         ];
 
         var container = document.getElementById('map');
@@ -39,7 +40,7 @@ function Flowrate() {
         var customOverlay = new kakao.maps.CustomOverlay({
             map: map,
             clickable: true,
-            content: '<div class="customOverlay">1</div>',
+            content: '<div class="customOverlay">' + value + '</div>',
             position: new kakao.maps.LatLng(36.35093930118267, 127.30023814907075),
             xAnchor: 0.5,
             yAnchor: 1,
@@ -49,11 +50,10 @@ function Flowrate() {
 
         for(var i = 0; i < overlayCoords.length; i++) {
             var temp = overlayCoords[i][0];
-            var className = overlayCoords[i][1] ? "customOverlayTurned" : "customOverlay";
             var res = new kakao.maps.CustomOverlay({
                 map: map,
                 clickable: true,
-                content: '<div class="' + className + '">1</div>',
+                content: '<div class="customOverlay">1</div>',
                 position: temp,
                 xAnchor: 0.5,
                 yAnchor: 1,
