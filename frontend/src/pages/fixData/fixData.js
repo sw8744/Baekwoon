95225ef300fd88e33a46993cf7a3e646b9b3db2f
thislife_won <sw8744@naver.com>
2024-11-07 03:57:28 +0900

    Finish(Maybe)

--- frontend/src/pages/fixData/fixData.js
+++ frontend/src/pages/fixData/fixData.js
@@ -1,14 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import './fixData.css';
 import refresh from '../../assets/refresh.svg';
-import Graph from "./graph";
-
 
 const { kakao } = window;
-var overlay;
+var overlay = [];
+var maxValue = [100, 150, 300, 50, 200, 75];
+
+const Graph = async (maxValue, currentValue) => {
+    var graphWidth = 20;
+    var graphHeight = 100;
+    var graphValue = Math.round((currentValue / maxValue) * 100);
+    var graphColor = '#FF0000';
+    if (graphValue < 50) {
+        graphColor = '#9FCE63';
+    } else if (graphValue < 80) {
+        graphColor = '#F8D648';
+    }
+    var graphValueFinal = graphValue;
+    if (graphValue > 100) {
+        graphValue = 100;
+    }
+    return ('<div id="graphRoot"><div id="graphText">' + maxValue + 'mm/h 수용 가능<br>(' + graphValueFinal + '%)</div><div class="graph" style="width:' + graphWidth + 'px;height:' + graphHeight + 'px; border: 1px solid #000066;"><div class="graphValue" style="width:' + graphWidth + 'px;height:' + graphValue + '%;background-color:' + graphColor + '; margin-top: ' + (graphHeight - graphValue) + 'px;"></div></div></div>');
+    
+}
 
 function Fixdata() {
+    const rainFallInput = useRef();
+
     const navigate = useNavigate();
     const goMain = () => {
         navigate('/');
@@ -20,6 +39,26 @@ function Fixdata() {
         navigate('/flowrate');
     };
 
+    const calculate = async (overlays) => {
+        var rainFall = await rainFallInput.current.value;
+        rainFall = parseInt(rainFall);
+        var contents = [];
+        for(var i = 0; i < overlays.length; i++) {
+            var maxSize = maxValue[i];
+            if(maxSize != undefined) {
+                await Graph(maxSize, rainFall).then((overlayContent) => {
+                    contents.push(overlayContent);
+                });
+            }
+        }
+        console.log(contents);
+        console.log(overlays);
+        if(contents != undefined) {
+            for(var i = 0; i < contents.length; i++) {
+                overlays[i].setContent('<div class="">' + contents[i] + '</div>');
+            }
+        }
+    };
     useEffect(() => {
         var container = document.getElementById('map');
         var options = {
@@ -27,18 +66,89 @@ function Fixdata() {
             level: 2
         };
         var map = new kakao.maps.Map(container, options);
-        var overlayContent = Graph(100, 10);
-        var customOverlay = new kakao.maps.CustomOverlay({
-            map: map,
-            clickable: true,
-            content: '<div class="">' + overlayContent + '</div>',
-            position: new kakao.maps.LatLng(36.35093930118267, 127.30023814907075),
-            xAnchor: 0.5,
-            yAnchor: 1,
-            zIndex: 2
+        Graph(maxValue[0], 10).then((overlayContent) => {
+            var customOverlay1 = new kakao.maps.CustomOverlay({
+                map: map,
+                clickable: true,
+                content: '<div class="">' + overlayContent + '</div>',
+                position: new kakao.maps.LatLng(36.35093930118267, 127.30023814907075),
+                xAnchor: 0.5,
+                yAnchor: 1,
+                zIndex: 2
+            });
+            overlay.push(customOverlay1);
+            customOverlay1.setMap(map);
         });
-        overlay = customOverlay;
-        customOverlay.setMap(map);
+        
+        Graph(maxValue[1], 10).then((overlayContent) => {
+            var customOverlay2 = new kakao.maps.CustomOverlay({
+                map: map,
+                clickable: true,
+                content: '<div class="">' + overlayContent + '</div>',
+                position: new kakao.maps.LatLng(36.35093930118267, 127.29906814907075),
+                xAnchor: 0.5,
+                yAnchor: 1,
+                zIndex: 2
+            });
+            overlay.push(customOverlay2);
+            customOverlay2.setMap(map);
+        });
+
+        Graph(maxValue[2], 10).then((overlayContent) => {
+            var customOverlay3 = new kakao.maps.CustomOverlay({
+                map: map,
+                clickable: true,
+                content: '<div class="">' + overlayContent + '</div>',
+                position: new kakao.maps.LatLng(36.34973930118267, 127.30006814907075),
+                xAnchor: 0.5,
+                yAnchor: 1,
+                zIndex: 2
+            });
+            overlay.push(customOverlay3);
+            customOverlay3.setMap(map);
+        });
+
+        Graph(maxValue[3], 10).then((overlayContent) => {
+            var customOverlay4 = new kakao.maps.CustomOverlay({
+                map: map,
+                clickable: true,
+                content: '<div class="">' + overlayContent + '</div>',
+                position: new kakao.maps.LatLng(36.35047930118267, 127.30093814907075),
+                xAnchor: 0.5,
+                yAnchor: 1,
+                zIndex: 2
+            });
+            overlay.push(customOverlay4);
+            customOverlay4.setMap(map);
+        });
+
+        Graph(maxValue[4], 10).then((overlayContent) => {
+            var customOverlay5 = new kakao.maps.CustomOverlay({
+                map: map,
+                clickable: true,
+                content: '<div class="">' + overlayContent + '</div>',
+                position: new kakao.maps.LatLng(36.3516430118267, 127.30189814907075),
+                xAnchor: 0.5,
+                yAnchor: 1,
+                zIndex: 2
+            });
+            overlay.push(customOverlay5);
+            customOverlay5.setMap(map);
+        });
+
+        Graph(maxValue[5], 10).then((overlayContent) => {
+            var customOverlay6 = new kakao.maps.CustomOverlay({
+                map: map,
+                clickable: true,
+                content: '<div class="">' + overlayContent + '</div>',
+                position: new kakao.maps.LatLng(36.3523430118267, 127.30025814907075),
+                xAnchor: 0.5,
+                yAnchor: 1,
+                zIndex: 2
+            });
+            overlay.push(customOverlay6);
+            customOverlay6.setMap(map);
+        });
     }, []);
 
     return (
@@ -51,8 +161,8 @@ function Fixdata() {
                     <button id="mapButton" className="mapButton" onClick={() => {}}><img src={refresh} id="refreshBtnImg" /></button>
                 </div>
                 <div id="settingDiv">
-                    <input type="text" id="settingInput" placeholder="예상 강수량..." />
-                    <button id="settingButton">설정</button>
+                    <input type="text" id="settingInput" placeholder="예상 강수량..." defaultValue={10} ref={rainFallInput} />
+                    <button id="settingButton" onClick={() => {calculate(overlay)}}>설정</button>
                 </div>
             </div>
         </div>
