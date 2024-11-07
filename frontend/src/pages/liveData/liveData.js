96ed1b591699b0276702cf2928fb499be8f5ecba
thislife_won <sw8744@naver.com>
2024-11-08 03:28:29 +0900

    Finally.. Finish

--- frontend/src/pages/liveData/liveData.js
+++ frontend/src/pages/liveData/liveData.js
@@ -12,16 +12,64 @@ import warning from "../../assets/pin/warning.png";
 import danger from "../../assets/pin/danger.png";
 import { useNavigate } from "react-router-dom";
 import refresh from "../../assets/refresh.svg";
+import manholeRed from "../../assets/manhole-red.svg";
+import manholeGreen from "../../assets/manhole-green.svg";
+import undergroundParking from "../../assets/floodedCar.svg";
+import X from "../../assets/X.png";
+import undergroundHouse from "../../assets/undergroundHouse.png";
+import entrance from "../../assets/entrance.png";
 
 const { kakao } = window;
+var maps;
+var overlay;
+var manhole;
 var pins = [];
-var messages = ["", "반지하 주택 많음", "지하차도가 있음", "상습 침수 지하주차장 有"];
+var manholes = [];
+var flowValue = [1, 2, 1, 1, 2, 2, 3, 3, 4, 5, 4, 3, 5, 4, 3, 3, 5, 4, 4, 5, 3, 3, 4, 5, 3, 2, 1, 2, 2, 1];
+const overlayCoords = [
+    // Block 1
+    [new kakao.maps.LatLng(36.35093930118267, 127.30003814907075)],
+    [new kakao.maps.LatLng(36.35093930118267, 127.29983814907075)],
+    [new kakao.maps.LatLng(36.35093930118267, 127.29963814907075)],
+    [new kakao.maps.LatLng(36.35093930118267, 127.29943814907075)],
+    [new kakao.maps.LatLng(36.35093930118267, 127.29923814907075)],
+    [new kakao.maps.LatLng(36.35093930118267, 127.2990794907075)],
+    [new kakao.maps.LatLng(36.35111930118267, 127.2990794907075)],
+    [new kakao.maps.LatLng(36.35131930118267, 127.2990794907075)],
+    [new kakao.maps.LatLng(36.35151930118267, 127.2990794907075)],
+    [new kakao.maps.LatLng(36.35171930118267, 127.2991494907075)],
+    [new kakao.maps.LatLng(36.35193930118267, 127.2992794907075)],
+    [new kakao.maps.LatLng(36.35216030118267, 127.2994294907075)],
+    [new kakao.maps.LatLng(36.35229530118267, 127.2996194907075)],
+    [new kakao.maps.LatLng(36.35232530118267, 127.2998194907075)],
+    [new kakao.maps.LatLng(36.35232530118267, 127.300044907075)],
+    [new kakao.maps.LatLng(36.35232736118267, 127.300254907075)],
+    [new kakao.maps.LatLng(36.35210036118267, 127.300254907075)],
+    [new kakao.maps.LatLng(36.35180036118267, 127.300254907075)],
+    [new kakao.maps.LatLng(36.35150036118267, 127.300254907075)],
+    [new kakao.maps.LatLng(36.35120036118267, 127.300254907075)],
+
+    // Block 2
+    [new kakao.maps.LatLng(36.35073930118267, 127.2990794907075)],
+    [new kakao.maps.LatLng(36.35048930118267, 127.2993194907075)],
+    [new kakao.maps.LatLng(36.35022930118267, 127.2995794907075)],
+    [new kakao.maps.LatLng(36.35002930118267, 127.2997794907075)],
+    [new kakao.maps.LatLng(36.34972930118267, 127.3000794907075)],
+    [new kakao.maps.LatLng(36.35002930118267, 127.3004294907075)],
+    [new kakao.maps.LatLng(36.350422930118267, 127.3009094907075)],
+    [new kakao.maps.LatLng(36.350822930118267, 127.3011694907075)],
+    [new kakao.maps.LatLng(36.351322930118267, 127.3015094907075)],
+    [new kakao.maps.LatLng(36.351622930118267, 127.3019094907075)],
+];
 
 function Livedata() {
     const navigate = useNavigate();
     const [guState, setGuState] = useState({"대덕구": 0, "유성구": 0, "동구": 0, "중구": 0, "서구": 0});
-    const [clickedButton, setClickedButton] = useState(-1);
+    const [clickedButton, setClickedButton] = useState(null);
+    const [isAbleToClick, setIsAbleToClick] = useState(false);
+    const [messages, setMessages] = useState(["", "반지하 주택 많음", "지하차도가 있음", "상습 침수 지하주차장 有"]);
     const pinImg = [new kakao.maps.MarkerImage(safe, new kakao.maps.Size(30, 62), {offset: new kakao.maps.Point(15, 62)}), new kakao.maps.MarkerImage(warning, new kakao.maps.Size(60, 75), {offset: new kakao.maps.Point(30, 75)}), new kakao.maps.MarkerImage(danger, new kakao.maps.Size(40, 70), {offset: new kakao.maps.Point(20, 70)})];
+    const markerImg = [new kakao.maps.MarkerImage(manholeGreen, new kakao.maps.Size(30, 30), {offset: new kakao.maps.Point(15, 30)}), new kakao.maps.MarkerImage(manholeRed, new kakao.maps.Size(30, 30), {offset: new kakao.maps.Point(15, 30)})];
 
     const colorCode = [['#39DE2A', '#A2FF99'], ['#FFC107', '#FFE400'], ['#DC3545', '#FF4848']]
 
@@ -54,7 +102,163 @@ function Livedata() {
 
     const inputMessage = async (index) => {
         var message = prompt("메시지를 입력하세요.");
-        messages[index] = message;
+        var temp = messages;
+        temp[index] = message;
+        setMessages(temp);
+    };
+
+    const goDetail = async (kakao, map) => {
+        setIsAbleToClick(true);
+        for(let i = 0; i < pins.length; i++) {
+            pins[i].setMap(null);
+        }
+        var flowRate = await fetchFlowRate(overlay);
+            var imageSrc;
+            var imageSize = new kakao.maps.Size(30, 30);
+            var imageOption = {offset: new kakao.maps.Point(15, 30)};
+            if(flowRate <= 2) {
+                imageSrc = manholeRed;
+            }
+            else if(flowRate <= 4) {
+                imageSrc = manholeGreen
+            }
+
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+                markerPosition = temp;
+            var customOverlay = new kakao.maps.Marker({
+                position: new kakao.maps.LatLng(36.35093930118267, 127.30023814907075),
+                image: markerImage,
+            });
+        customOverlay.setMap(map);
+        manhole = customOverlay;
+
+        for(var i = 0; i < overlayCoords.length; i++) {
+            var temp = overlayCoords[i][0];
+            var imageSrc;
+            var imageSize = new kakao.maps.Size(30, 30);
+            var imageOption = {offset: new kakao.maps.Point(15, 30)};
+            if(flowValue[i] <= 2) {
+                imageSrc = manholeRed;
+            }
+            else if(flowValue[i] <= 4) {
+                imageSrc = manholeGreen
+            }
+
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+                markerPosition = temp;
+            var res = new kakao.maps.Marker({
+                position: markerPosition,
+                image: markerImage,
+            });
+            manholes.push(res);
+            res.setMap(map);
+        }
+        var imageSrc = undergroundParking;
+        var imageSize = new kakao.maps.Size(30, 30);
+        var imageOption = {offset: new kakao.maps.Point(15, 30)};
+
+        var undergroundPos = [
+            new kakao.maps.LatLng(36.35135222302956, 127.30008602142335),
+            new kakao.maps.LatLng(36.35146455563928, 127.29876101017),
+            new kakao.maps.LatLng(36.35180587295905, 127.30194211006166),
+            new kakao.maps.LatLng(36.35086832685774, 127.30204403400423),
+        ]
+
+        for(let i = 0; i < undergroundPos.length; i++) {
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+                markerPosition = undergroundPos[i];
+            var res = new kakao.maps.Marker({
+                position: markerPosition,
+                image: markerImage,
+            });
+            res.setMap(map);
+        }
+
+        var imageSrc = X;
+        var imageSize = new kakao.maps.Size(30, 30);
+        var imageOption = {offset: new kakao.maps.Point(15, 15)};
+
+        var trafficJamPos = [
+            new kakao.maps.LatLng(36.3509460960882, 127.29985535144807),
+        ]
+
+        for(let i = 0; i < trafficJamPos.length; i++) {
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+                markerPosition = trafficJamPos[i];
+            var res = new kakao.maps.Marker({
+                position: markerPosition,
+                image: markerImage,
+            });
+            res.setMap(map);
+        }
+        
+        var imageSrc = undergroundHouse;
+        var imageSize = new kakao.maps.Size(50, 50);
+        var imageOption = {offset: new kakao.maps.Point(25, 25)};
+
+        var undergroundPos = [
+            new kakao.maps.LatLng(36.35198301173768, 127.29997873306276),
+            new kakao.maps.LatLng(36.350561569691195, 127.29995727539064),
+        ]
+
+        for(let i = 0; i < undergroundPos.length; i++) {
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+                markerPosition = undergroundPos[i];
+            var res = new kakao.maps.Marker({
+                position: markerPosition,
+                image: markerImage,
+            });
+            res.setMap(map);
+        }
+
+        var imageSrc = entrance;
+        var imageSize = new kakao.maps.Size(25, 50);
+        var imageOption = {offset: new kakao.maps.Point(25, 12.5)};
+
+        var entrancePos = [
+            new kakao.maps.LatLng(36.351628733777325, 127.30195283889772),
+            new kakao.maps.LatLng(36.350967698638435, 127.29928672313692),
+        ]
+
+        for(let i = 0; i < entrancePos.length; i++) {
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+                markerPosition = entrancePos[i];
+            var res = new kakao.maps.Marker({
+                position: markerPosition,
+                image: markerImage,
+            });
+            res.setMap(map);
+        }
+
+        kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+            var imageSrc = X;
+            var imageSize = new kakao.maps.Size(30, 30);
+            var imageOption = {offset: new kakao.maps.Point(15, 15)};
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+                markerPosition = mouseEvent.latLng;
+            var res = new kakao.maps.Marker({
+                position: markerPosition,
+                image: markerImage,
+            });
+            res.setMap(map);
+        });
+    }
+
+    const fetchFlowRate = async () => {
+        const response = await fetch('http://127.0.0.1:5000/api/getSensorInfo');
+        const data = await response.json();
+        const dataOverlay = await data["1"].state;
+        return dataOverlay;
+    };
+
+    const fetchManholeInfo = async (manhole) => {
+        const response = await fetchFlowRate();
+        if(response <= 2) {
+            manhole.setImage(markerImg[0]);
+        }
+        else if(response <= 5) {
+            manhole.setImage(markerImg[1]);
+        }
     };
 
     const goMain = () => {
@@ -70,12 +274,15 @@ function Livedata() {
     };
 
     useEffect(() => {
+        setClickedButton(null);
+        setIsAbleToClick(false);
         var container = document.getElementById('map');
         var options = {
             center: new kakao.maps.LatLng(36.3513473654900, 127.30094246537497),
             level: 13
         };
         var map = new kakao.maps.Map(container, options);
+        maps = map;
         var polygonPathDaejeon = [];
         var polygonPathYuseong = [];
         var polygonPathDaedeok = [];
@@ -286,10 +493,13 @@ function Livedata() {
                         kakao.maps.event.addListener(pin, 'click', function() {
                             if(clickedButton != i) {
                                 setClickedButton(i);
+                                return;
                             }
-                            else {
-                                setClickedButton(-1);
+                            else if(clickedButton === i) {
+                                setClickedButton(null);
+                                return;
                             }
+                            console.log(clickedButton);
                         });
                     }
                 });
@@ -337,7 +547,7 @@ function Livedata() {
                 element.setMap(map);
             });
         });
-
+        
         kakao.maps.event.addListener(polygonSeo, 'click', async function(mouseEvent) {  
             map.setCenter(new kakao.maps.LatLng(36.32652012072702, 127.38557556152345));
             map.setLevel(9);
@@ -458,16 +668,16 @@ function Livedata() {
                     <button id="mapButton" className="mapButtonClicked" onClick={() => {goMain()}}>실시간 배수로 데이터</button>
                     <button id="mapButton" className="mapButton" onClick={() => {goFlowRate()}}>배수 유속관리 시스템</button>
                     <button id="mapButton" className="mapButton" onClick={() => {goFixData()}}>도시 정비계획 데이터</button>
-                    <button id="mapButton" className="mapButton" onClick={() => {for(var i = 0; i < pins.length; i++) {fetchPinInfo(pins[i], i + 1);}}}><img src={refresh} id="refreshBtnImg" /></button>
+                    <button id="mapButton" className="mapButton" onClick={() => {for(var i = 0; i < pins.length; i++) {fetchPinInfo(pins[i], i + 1);}; fetchManholeInfo();}}><img src={refresh} id="refreshBtnImg" /></button>
                 </div>
                 {
-                    clickedButton != -1
+                    clickedButton != null
                     ? <div id="infoDiv">
                     <div id="infoTitle">PIN #{clickedButton}</div>
-                    <div id="infoContent">{messages[1]}</div>
+                    <div id="infoContent">{messages[clickedButton]}</div>
                     <div id="infoButtonDiv">
-                        <button id="infoButton" className="infoButton">입력</button>
-                        <button id="infoButton" className="infoButton">수정</button>
+                        <button id="infoButton" className="infoButton" onClick={() => {inputMessage(clickedButton)}}>입력</button>
+                        <button id="infoButton" className="infoButton" onClick={() => {goDetail(kakao, maps)}}>상세</button>
                     </div>
                 </div>
                 : null
